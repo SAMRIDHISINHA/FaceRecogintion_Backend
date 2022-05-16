b78a31aa730c98a71dd0c21f1d15a5417712fc21
SAMRIDHISINHA <samridhisinha1234@gmail.com>
2022-05-16 10:08:09 +0530

    Initial Commit

---
+++ virtualenv/facedec.py
@@ -0,0 +1,31 @@
+import cv2
+
+#importing some preexsiting algorithm 
+trained_face_data=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#Chose image to detect
+#img=cv2.imread('rdj.jpeg')
+
+webcam=cv2.VideoCapture(0)
+
+
+while True:
+
+    #frame read
+    successful_frame_read, frame=webcam.read()
+
+
+    #Converting image to grayscale
+    grayscaled_img= cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+
+    #Detect faces
+    face_coordinates=trained_face_data.detectMultiScale(grayscaled_img)
+
+
+    #Draw rectange around the faces
+    for (x,y,w,h) in face_coordinates:
+      cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 2)
+
+    cv2.imshow('RDJ',frame)
+    cv2.waitKey(1)
